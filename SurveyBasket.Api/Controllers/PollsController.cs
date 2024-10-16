32f140d14e45c117c8882a9785ddd0d29c706d93
mohamedshehatadev17 <shetta1999@gmail.com>
2024-10-16 22:24:13 +0300

    Add Crud operation to Polls Controller

---
+++ SurveyBasket.Api/Controllers/PollsController.cs
@@ -0,0 +1,68 @@
+﻿
+using SurveyBasket.Api.Services;
+
+namespace SurveyBasket.Api.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PollsController : ControllerBase
+    {
+        private readonly IPollService _pollService;
+        public PollsController(IPollService pollService)
+        {
+            _pollService = pollService;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var polls=_pollService.GetAll();
+            return Ok(polls);
+        }
+        [HttpGet("{id:int}")] 
+        public IActionResult Get(int id)
+        {
+            var Poll = _pollService.Get(id);
+
+            return Poll is null ? NotFound():Ok(Poll);
+        }
+        [HttpPost]
+        public IActionResult Add(Poll poll)
+        {
+            var newPoll= _pollService.Add(poll);
+            return CreatedAtAction(nameof(Get), new { id = newPoll.Id }, newPoll);
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult Update(int id,Poll poll)
+        {
+            var isUpdated = _pollService.Update(id,poll);
+            if (!isUpdated)
+                return NotFound();
+            
+            return NoContent();
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var isDeleted =_pollService.Delete(id);
+            if (!isDeleted)
+                return NotFound();
+            return NoContent();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ SurveyBasket.Api/Services/PollService.cs
@@ -0,0 +1,48 @@
+﻿
+using Microsoft.AspNetCore.Http.HttpResults;
+using SurveyBasket.Api.Models;
+using System.Collections.Generic;
+
+namespace SurveyBasket.Api.Services
+{
+    public class PollService : IPollService
+    {
+
+        private readonly static List<Poll> _polls = [
+        new Poll{
+                        Id = 1,
+                        Title = "tile",
+                        Description ="des1"
+            }
+        ];
+        public Poll Add(Poll poll)
+        {
+            poll.Id = _polls.Count + 1;
+            _polls.Add( poll );
+            return ( poll );
+        }
+
+        public Poll? Get(int id) =>_polls.SingleOrDefault(p => p.Id == id);
+        public IEnumerable<Poll> GetAll()=>_polls;
+
+        public bool Update(int id,Poll poll)
+        {
+            var currentPool=Get(id);
+            if (currentPool is null)
+                return false;
+            currentPool.Title = poll.Title;
+            currentPool.Description = poll.Description;
+            return true;
+
+        }
+        public bool Delete(int id)
+        {
+            var poll=Get(id);
+            if (poll is null)
+                return false;
+            _polls.Remove( poll );
+
+            return true;
+        }
+    }
+}
