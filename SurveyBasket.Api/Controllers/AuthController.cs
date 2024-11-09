d537648e859b0e243cb81ffaec9f9039334df459
mohamedshehatadev17 <mohamedshehatadev0@gmail.com>
2024-11-09 17:00:55 +0200

    end of refresh token  ## need to be tested when Register is Done

--- SurveyBasket.Api/Controllers/AuthController.cs
+++ SurveyBasket.Api/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
 ﻿
+using Microsoft.AspNetCore.Mvc;
+
 namespace SurveyBasket.Api.Controllers
 {
 	[Route("[controller]")]
@@ -9,18 +11,25 @@ namespace SurveyBasket.Api.Controllers
         private readonly IAuthService _authService = authService;
 
         [HttpPost("Login")]
-		public async Task<IActionResult> LoginAsync(LoginRequest request,CancellationToken cancellationToken)
+		public async Task<IActionResult> LoginAsync([FromBody]LoginRequest request,CancellationToken cancellationToken)
 		{
 			var authResult = await _authService.GetTokenAsync(request.Email, request.Password, cancellationToken);
 			
 			return authResult is null ? BadRequest("Invalid Email/Password"):Ok(authResult);
 		}
         [HttpPost("refresh")]
-        public async Task<IActionResult> RefreshAsync(RefreshTokenRequest request, CancellationToken cancellationToken)
+        public async Task<IActionResult> RefreshAsync([FromBody] RefreshTokenRequest request, CancellationToken cancellationToken)
         {
             var authResult = await _authService.GetRefreshTokenAsync(request.Token, request.RefreshToken, cancellationToken);
 
             return authResult is null ? BadRequest("Invalid token") : Ok(authResult);
+        }
+        [HttpPost("revoke-refresh-token")]
+        public async Task<IActionResult> RevokeRefreshTokenAsync([FromBody] RefreshTokenRequest request, CancellationToken cancellationToken)
+        {
+            var isRevoked = await _authService.RevokeRefreshTokenAsync(request.Token, request.RefreshToken, cancellationToken);
+
+            return isRevoked ? Ok() : BadRequest("Operation failed");
         }
     }
 }
--- SurveyBasket.Api/Services/IAuthService.cs
+++ SurveyBasket.Api/Services/IAuthService.cs
@@ -5,5 +5,6 @@ namespace SurveyBasket.Api.Services
 	{
 		Task<AuthResponse?> GetTokenAsync(string email, string password, CancellationToken cancellationToken = default);
 		Task<AuthResponse?> GetRefreshTokenAsync(string token, string refreshToken, CancellationToken cancellationToken = default);
+		Task<bool> RevokeRefreshTokenAsync(string token, string refreshToken, CancellationToken cancellationToken = default);
 	}
 }
