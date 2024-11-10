41b092539067c5548f86e3ff11beec74c922a4e6
mohamedshehatadev17 <mohamedshehatadev0@gmail.com>
2024-11-10 22:23:46 +0200

    config Result Pattern

---
+++ SurveyBasket.Api/Abstractions/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace SurveyBasket.Api.Abstractions
+{
+    public record Error(string Code,string Description)
+    {
+        public static readonly Error None =new(string.Empty,string.Empty);
+
+    }
+}
